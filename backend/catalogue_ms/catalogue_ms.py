0ab48a8349beb702f7971f3cf945bc6ee05ca00d
Lorenzo <aloisi.lorenzo99@gmail.com>
2023-03-11 17:23:17 +0100

    catalogue microservice almost done

--- backend/catalogue_ms/catalogue_ms.py
+++ backend/catalogue_ms/catalogue_ms.py
@@ -1,13 +1,11 @@
 from flask import Flask 
 from flask import jsonify
 import mysql.connector
-
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-
+def dbConnect():
     #MySQL connection config
     config = {
         'user' : 'root',
@@ -17,19 +15,56 @@ def main():
         'database' : 'rentYourExpert',
     }
     db = mysql.connector.connect(**config)
+    return db
 
+@app.route('/catalogue', methods=['GET'])
+def getAllWorkers():
+    #connecting to the database
+    db = dbConnect()
+
+    query = "SELECT name,surname,profession,location,description,phone,available FROM user WHERE isWorker=1"
     #executing the query
     cursor = db.cursor()
-    cursor.execute("SELECT * from user")
-    result = cursor.fetchall()
+    cursor.execute(query)
+
+    #jsonifying 
+    row_headers = [x[0] for x in cursor.description] #this will extract row headers
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+
+    #closing the connection to the database
     cursor.close()
     db.close()
-    response = "Hello Daniele \n"
 
-    for x in result: 
-        response += str(x) + '\n'
+    return json.dumps(json_data)
 
-    return response
+
+@app.route('/catalogue/<id>', methods=['GET'])
+def getWorker(id):
+    #connecting to the database
+    db = dbConnect()
+
+    query = "SELECT name,surname,profession,location,description,phone,available FROM user WHERE isWorker=1 AND id={id}".format(id=id)
+    #executing the query
+    cursor = db.cursor()
+    cursor.execute(query)
+
+    #jsonifying 
+    row_headers = [x[0] for x in cursor.description] #this will extract row headers
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+
+    #closing the connection to the database
+    cursor.close()
+    db.close()
+
+    return json.dumps(json_data)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0', port = 5000)
