e5e978c5a0c7ebdf157dbe2e9a664d9d4cfb2d7c
Lorenzo <aloisi.lorenzo99@gmail.com>
2023-03-14 19:41:15 +0100

    fixing db

--- backend/management_ms/management_ms.py
+++ backend/management_ms/management_ms.py
@@ -75,7 +75,7 @@ def addCustomer():
 
 
 @app.route('/customers/<id>', methods=['DELETE'])
-def delUser(id):
+def delCustomer(id):
     
     #connecting to the database
     db = dbConnect()
@@ -91,6 +91,52 @@ def delUser(id):
     db.close()
 
     return str(id),200
+
+@app.route('/workers', methods=['GET'])
+def getAllWorkers():
+    #connecting to the database
+    db = dbConnect()
+
+    query = "SELECT * FROM worker "
+    #executing the query
+    cursor = db.cursor()
+    cursor.execute(query)
+
+    #jsonifying 
+    row_headers = [x[0] for x in cursor.description] #this will extract row headers
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+
+    #closing the connection to the database
+    cursor.close()
+    db.close()
+
+    return json.dumps(json_data)
+
+@app.route('/workers/<id>', methods=['GET'])
+def getWorker(id):
+    #connecting to the database
+    db = dbConnect()
+
+    query = "SELECT * FROM worker WHERE id={id}".format(id=id)
+    #executing the query
+    cursor = db.cursor()
+    cursor.execute(query)
+
+    #jsonifying 
+    row_headers = [x[0] for x in cursor.description] #this will extract row headers
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+
+    #closing the connection to the database
+    cursor.close()
+    db.close()
+
+    return json.dumps(json_data)
 
 
 @app.route('/workers', methods=['POST'])
