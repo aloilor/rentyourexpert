44c3c47bd545f7d48a12906a7369d22845eb02f1
elebearossi <rossi.1749038@studenti.uniroma1.it>
2023-03-19 19:20:06 +0100

    details

--- frontend/frontend-app/src/Components/AdminPanel/AdminPanel.js
+++ frontend/frontend-app/src/Components/AdminPanel/AdminPanel.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LogoutCustomerButton from '../Logout/Logout';
 
 function AdminPanel() {
   return (
     <div>
+       <LogoutCustomerButton />
       <Link to="/admin/customers">
         <button>Customers</button>
       </Link>
